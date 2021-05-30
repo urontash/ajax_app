34138ac092ef3cac3a65603e840106dee344ea71
urontash <m86.nakajima.shuntaro@gmail.com>
2021-05-30 20:46:09 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  
+  private
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # username == 'admin' && password =='2222'
+      # 環境変数を読み込む設定へ変更
+      # binding.pry
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
